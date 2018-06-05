7ce3ed977edb0a799a28c222d0b7bb818c83683b
Dennis Padiernos <depadiernos@gmail.com>
2018-06-05 18:02:29 -0500

    Remove list-style for <summary> and change the cursor to pointer to make it obvious that it's clickable

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -5,7 +5,14 @@ import getPost from '../utilities/getPosts'
 
 //style
 const Details = styled('details')`
-  margin: 0 1em;
+  margin: 0 3em;
+  summary {
+    list-style: none;
+    cursor: pointer;
+    &::-webkit-details-marker {
+    display: none;
+  }
+  }
 `
 
 //component
@@ -18,7 +25,7 @@ export default () => {
         (post, index) =>
         <Details key={index}>
           <summary>
-            {posts[post].data.title}
+            <h2>{posts[post].data.title}</h2>
           </summary>
           <ReactMarkdown
             source={posts[post].content}
