d8f3a7a975a619d773d96e2feb76907142c62e6c
Dennis Padiernos <depadiernos@gmail.com>
2018-06-05 14:27:51 -0500

    clean up content code

--- src/utilities/getPosts.js
+++ src/utilities/getPosts.js
@@ -2,9 +2,7 @@ import matter from 'gray-matter'
 import md from '../content/posts/*.md'
 
 export default () => {
-  console.log(md)
   let posts = {}
   posts = Object.keys(md).map( post => matter(md[post]))
-  console.log(posts)
   return posts
 }
