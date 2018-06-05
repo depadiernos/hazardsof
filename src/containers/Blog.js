e6bebb510c0d17975127446d7d1ea41b86e07da2
Dennis Padiernos <depadiernos@gmail.com>
2018-06-05 17:53:39 -0500

    adjust margin for blog page

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,9 +1,14 @@
 import React, { Fragment } from 'react'
 import styled from 'react-emotion'
 import ReactMarkdown from 'react-markdown'
-
 import getPost from '../utilities/getPosts'
 
+//style
+const Details = styled('details')`
+  margin: 0 1em;
+`
+
+//component
 export default () => {
   const posts = getPost()
   console.log(posts)
@@ -11,14 +16,14 @@ export default () => {
     <Fragment>
       {Object.keys(posts).map(
         (post, index) =>
-        <details key={index}>
+        <Details key={index}>
           <summary>
             {posts[post].data.title}
           </summary>
           <ReactMarkdown
             source={posts[post].content}
           />
-        </details>
+        </Details>
       )}
     </Fragment>
   )
