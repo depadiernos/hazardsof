c82781a98bf158af3bef6bbec0b63303522a0ad6
Dennis Padiernos <depadiernos@gmail.com>
2019-12-20 06:44:06 -0600

    monorepo test

---
+++ monorepo/src/index.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from 'react'
+import { render } from 'react-dom'
+import { Router } from '@reach/router'
+import styled, { injectGlobal } from 'react-emotion'
+//import stateless components
+import Header from './components/header'
+import Footer from './components/footer'
+//import containers
+import Home from './containers/Home'
+import About from './containers/About'
+import Blog from './containers/Blog'
+
+const App = () => {
+  return (
+    <Body>
+      <Header/>
+      <RouterDiv>
+        <Home path="/"/>
+        <About path="/about"/>
+        <Blog path="/blog" />
+      </RouterDiv>
+      <Footer/>
+    </Body>
+  )
+}
+
+injectGlobal`
+  body {
+    margin: 0;
+  }
+`
+
+const Body = styled('div')`
+  font-family: Sans;
+  margin: 0 auto;
+`
+const RouterDiv = styled(Router)`
+  padding-top: 52px;
+`
+
+render(<App />, document.getElementById('root'));
