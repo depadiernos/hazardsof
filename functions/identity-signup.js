230a9f6bb364398d65de10c4aae48451fc45f576
Dennis Padiernos <depadiernos@gmail.com>
2024-11-21 16:19:15 -0600

    Update identity-signup.js

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -4,8 +4,7 @@ exports.handler = async function(event, context) {
  console.log("Signup from invite detected")
   return {
     statusCode: 200,
-    body: { message: "Signup from invite detected"}
+    body: JSON.stringify({ message: "Signup from invite detected"})
    }
   }
-  // your server-side functionality
 };
