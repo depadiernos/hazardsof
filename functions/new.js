888a008824886793ba447c337db682a2de976c46
Dennis Padiernos <depadiernos@gmail.com>
2024-11-27 12:11:53 -0600

    Update new.js

--- functions/new.js
+++ functions/new.js
@@ -17,16 +17,3 @@ export default async function(req, context) {
   const url = new URL(req.url);
   return new Response('hello, new here', { headers });
 }
-
-
-export default async (req: Request, context: Context) => {
-  const construction = getStore("construction");
-  await construction.set("nails", "For general carpentry");
-
-  const beauty = getStore("beauty");
-  await beauty.set("nails", "For formal events", {
-    metadata: { material: "acrylic", sale: true },
-  });
-
-  return new Response("Nail blobs set for Construction and Beauty stores");
-};
